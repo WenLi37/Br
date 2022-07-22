a356505b772610674f98a78a401ded3ab7b5c5d7
Lask LK <lhxlive@qq.com>
2022-07-22 09:53:17 +0800

    添加项目文件。

---
+++ Web/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Web {
+	partial class Main_Form {
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+			this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+			this.chromiumHostControl1 = new CefSharp.WinForms.Host.ChromiumHostControl();
+			this.SuspendLayout();
+			// 
+			// chromiumWebBrowser1
+			// 
+			resources.ApplyResources(this.chromiumWebBrowser1, "chromiumWebBrowser1");
+			this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+			this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+			// 
+			// chromiumHostControl1
+			// 
+			resources.ApplyResources(this.chromiumHostControl1, "chromiumHostControl1");
+			this.chromiumHostControl1.Name = "chromiumHostControl1";
+			this.chromiumHostControl1.Click += new System.EventHandler(this.chromiumHostControl1_Click);
+			// 
+			// Main_Form
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AllowDrop = true;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.chromiumHostControl1);
+			this.Controls.Add(this.chromiumWebBrowser1);
+			this.DoubleBuffered = true;
+			this.Name = "Main_Form";
+			this.Load += new System.EventHandler(this.Main_Form_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+		private CefSharp.WinForms.Host.ChromiumHostControl chromiumHostControl1;
+	}
+}
+
---
+++ Web/Form1.cs
@@ -0,0 +1,31 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Windows.Forms;
+
+
+namespace Web {
+	public partial class Main_Form : Form {
+		Web.Properties.Settings settings = new Web.Properties.Settings();
+		public ChromiumWebBrowser CWB;
+		public void InitBrowser() {
+			Cef.Initialize(new CefSettings());
+			CWB = new ChromiumWebBrowser("baidu.com");
+			this.Controls.Add(CWB);
+			CWB.Dock = DockStyle.Fill;
+		}
+
+
+		public Main_Form() {
+			InitBrowser();
+			InitializeComponent();
+		}
+
+		private void chromiumHostControl1_Click(object sender, EventArgs e) {
+
+		}
+
+		private void Main_Form_Load(object sender, EventArgs e) {
+		}
+	}
+}
